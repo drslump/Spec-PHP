adea821f35f44d128591dba9f56d569ec24706a9
drslump <imv@tid.es>
2011-03-09 01:07:32 +0100

    Added example tests

---
+++ tests/AllTests.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once __DIR__ . '/../library/Spec.php';
+
+// By extending this class all specs in the current directory will be runs
+class AllTests extends \DrSlump\Spec\PHPUnit\DirectorySpecTests
+{
+}
+
